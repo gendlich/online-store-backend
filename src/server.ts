8112d56858d7faeb0336348d8200a6ae81bbfac3
gendlich <g.endlich.g@gmail.com>
2022-03-23 22:55:37 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import router from './routes/Router';
 
-const app: express.Application = express()
-const port: Number = 3000;
+const app: express.Application = express();
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+app.use('/', router)
+
 
 app.listen(port, function () {
-    console.log(`starting app on the ${port} port`)
-})
+  console.log(`starting app on the ${port} port`);
+});
